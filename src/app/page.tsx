29fdd5dbe6f44711fc9fe6a744d1e5bcabd05264
KimAllise32 <kacummings@wpi.edu>
2025-10-31 14:56:49 -0400

    added some styling and made it so login connects to applicant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   const router = useRouter();
   const goToRegisterApplicant = () => router.push('/applicant/register');
   const goToHome = () => router.push('/');
+  const goToApplicantHome = () => router.push("/applicant/homepage");
 
   React.useEffect(() => {
   }, [model, redraw])
@@ -76,7 +77,7 @@ export default function Home() {
         
           <input className="inputBox" placeholder="Username" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
           <input className="inputBox" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-          <button className="bigButton">Login</button>
+          <button className="bigButton" onClick={goToApplicantHome}>Login</button>
           <button className="bigButton" onClick={(e) => goToRegisterApplicant()}>Register Account</button>
 
           <button id="lambdaTestButton" className="bigButton" onClick={(e) => testLambda()}>Test Lambda Function</button>
