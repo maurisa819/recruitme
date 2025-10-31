4a4fed33d6e76e331ca97fae6a674bf95f21e639
chooboy <chooboy05@gmail.com>
2025-10-31 14:27:48 -0400

    Company log out functionality

--- src/app/company/home/page.tsx
+++ src/app/company/home/page.tsx
@@ -51,6 +51,7 @@ export default function CompanyHome() {
   return (
     <div className="company-home">
       <div className="ribbon">
+        <button className="ribbonButton" onClick={(e) => logout()}>Logout</button>
         <img
           className="ribbonImages"
           src="/recruitme.png"
